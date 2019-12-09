6d7796ffa7f27fc132125073018bddab713d29a6
Ahmad Wilson <ahwilson712@gmail.com>
2019-12-08 22:31:03 -0500

    add package installation functionality and more testing

--- test/integration/install/gem_install/default_test.rb
+++ test/integration/install/gem_install/default_test.rb
@@ -1,1 +1,10 @@
-title "Default role installation integration test suite"
+title "Default role gem installation integration test suite"
+
+describe file('/usr/local/bin/fluentd') do
+  it { should exist }
+end
+
+describe gem('fluentd') do
+  it { should be_installed }
+  its('version') { should eq '1.7.3' }
+end
